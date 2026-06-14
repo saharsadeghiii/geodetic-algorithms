4b434e739f606debe5dc3cb3ee53b95d18161312
sahaesadeghiii <saharsadeghi.ek@gmail.com>
2026-06-14 10:33:27 +0200

    "BFS implemented"

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # algorithms
+
+# BFS - Breadth First Search
+## complexity
+is algorithm for traversing a graph G = (V,E), which require  time.
+- Time: O(|V| + |E|)
+- Space: O(|V|)
--- graph/bfs.py
+++ graph/bfs.py
@@ -1,1 +1,33 @@
 #BSF Algorithm
+
+from collections import deque
+
+def BFS(graph: dict, start) -> list:
+    if start not in graph:
+        raise ValueError(f"Node '{start}' is not exist is graph")
+
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    result = []
+
+    while queue:
+        node = queue.popleft()
+        result.append(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+    return result
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print(BFS(graph, 'B'))
